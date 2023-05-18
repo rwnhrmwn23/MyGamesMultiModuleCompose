1a2943ee4ce34babce4078b344f217a77e8d429a
Irwan Hermawan <rwnhermawan@gmail.com>
2023-05-18 16:36:00 +0700

    Initial commit - add version catalogue

---
+++ buildSrc/src/main/java/Configs.kt
@@ -0,0 +1,9 @@
+object Configs {
+    const val compileSdk = 33
+    const val minSdk = 24
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val nameSpace = "com.onedev.mygamesmultimodulecompose"
+    const val applicationId = "com.onedev.mygamesmultimodulecompose"
+}
